6694790fbaceca820bd279abec024085afebb1a1
moteele <daniel.rychly@posteo.cz>
2022-06-14 22:53:24 +0200

    cosmetic changes

--- lib/screens/reject.dart
+++ lib/screens/reject.dart
@@ -8,32 +8,36 @@ class Reject extends StatelessWidget {
   Widget build(BuildContext context) {
     Box<Syllable> sylBox = Get.find();
     Syllable? syl = sylBox.get(Get.arguments);
+
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const Text('Chceš to zkusit znovu?',
-            style: TextStyle(fontSize: 30, color: Colors.white)),
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                primary: const Color(0xff29CFD6),
-                minimumSize: Size(MediaQuery.of(context).size.width / 2, 80)),
-            onPressed: () {
-              Get.toNamed('/speak', arguments: Get.arguments);
-            },
-            child: const Text('Ano', style: TextStyle(fontSize: 30))),
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                primary: const Color(0xff29CFD6),
-                minimumSize: Size(MediaQuery.of(context).size.width / 2, 80)),
-            onPressed: () {
-              syl?.index++;
-              sylBox.put(syl!);
-              Get.offAndToNamed('/dashboard');
-            },
-            child: const Text('Ne', style: TextStyle(fontSize: 30))),
-      ],
-    ));
+      appBar: AppBar(title: const Text('Validace')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Chceš to zkusit znovu?',
+                style: TextStyle(fontSize: 30, color: Colors.white)),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: const Color(0xff29CFD6),
+                    fixedSize: Size(MediaQuery.of(context).size.width / 2, 80)),
+                onPressed: () {
+                  Get.toNamed('/speak', arguments: Get.arguments);
+                },
+                child: const Text('Ano', style: TextStyle(fontSize: 30))),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: const Color(0xff29CFD6),
+                    fixedSize: Size(MediaQuery.of(context).size.width / 2, 80)),
+                onPressed: () {
+                  syl?.index++;
+                  sylBox.put(syl!);
+                  Get.offAndToNamed('/dashboard');
+                },
+                child: const Text('Ne', style: TextStyle(fontSize: 30))),
+          ],
+        ),
+      ),
+    );
   }
 }
